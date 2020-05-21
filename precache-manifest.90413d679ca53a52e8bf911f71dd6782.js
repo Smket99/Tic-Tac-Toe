619b7c92632b40c52ac6a232171f261027b7c147
smket99 <f20180231@hyderabad.bits-pilani.ac.in>
2020-05-21 16:47:48 +0530

    Updates

---
+++ precache-manifest.90413d679ca53a52e8bf911f71dd6782.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00c77a4b44da5dced6d946b970c245f6",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "fab36ca87467942404ed",
+    "url": "/tic-tac-toe/static/css/main.99760e92.chunk.css"
+  },
+  {
+    "revision": "19511514435aa557817c",
+    "url": "/tic-tac-toe/static/js/2.07ab94ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.07ab94ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fab36ca87467942404ed",
+    "url": "/tic-tac-toe/static/js/main.6c6ba380.chunk.js"
+  },
+  {
+    "revision": "048e937512bfa9f4fb3a",
+    "url": "/tic-tac-toe/static/js/runtime-main.cb3e9845.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tic-tac-toe/static/media/logo.5d5d9eef.svg"
+  }
+]);
